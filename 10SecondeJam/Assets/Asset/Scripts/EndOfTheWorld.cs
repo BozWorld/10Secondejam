52d554c2b226e2c3417016713c7ca23babde0b95
Hap33 <36961317+Hap33@users.noreply.github.com>
2020-11-01 18:12:12 +0100

    Rework Menu + Build 2 Electric Bogaloo

---
+++ 10SecondeJam/Assets/Asset/Scripts/EndOfTheWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfTheWorld : MonoBehaviour
+{ 
+    void Start()
+    {
+        StartCoroutine(KillTheWorld());
+    }
+
+    private IEnumerator KillTheWorld()
+    {
+        yield return new WaitForSeconds(6);
+        Application.Quit();
+    }
+    
+}
